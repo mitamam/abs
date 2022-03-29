38a5d8922fa5179abb2584544c7bb4621e48599d
Mitama Masuda <mitama.md@gmail.com>
2022-03-29 18:23:18 +0900

    End Card Game for Two

--- abc088b/main.cpp
+++ abc088b/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-	
+	int16_t N, A, B;
 
+	A = B = 0;
+	std::cin >> N;
+	std::vector<int16_t> a(N);
+	for (int16_t i = 0; i < N; i++)
+		std::cin >> a[i];
+	std::sort(a.begin(), a.end());
+	for (int16_t i = 1; i <= N ; i++)
+		if (i % 2 == 1)
+			A += a[N - i];
+		else
+			B += a[N - i];
+	std::cout << A - B << std::endl;
 	return (0);
 }
